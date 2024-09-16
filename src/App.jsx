31a7ca7751ddde2bb4906323dd01b10635003014
Bảo Trần <baohse321@gmail.com>
2024-09-16 21:19:35 +0700

    test-commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { Alert } from "./components/ui/alert";
 
 function App() {
   return (
     <>
       <div className=" justify-center items-center flex h-screen">
         <Button>Click me</Button>
+        <Alert type="success">This is a success alert</Alert>
       </div>
     </>
   );
