a62d7d2354a04945065c176c41493bfa1ac56891
khoipham226 <113128599+khoipham226@users.noreply.github.com>
2024-09-18 20:07:36 +0700

    create header layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
 import { CourseLandingPageJsx } from "./components/course-landing-page";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Updated import
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Outlet,
+} from "react-router-dom";
 import Login from "./components/Login";
+import CoursesPage from "./components/Courses";
+import SubjectDetailsPage from "./components/CourseDetails";
+import { Layout } from "./components/Layout";
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/* Route for login, without the layout */}
         <Route path="/login" element={<Login />} />
-        <Route path="/" element={<CourseLandingPageJsx />} />
+
+        {/* Protected routes using the layout */}
+        <Route
+          element={
+            <Layout>
+              <Outlet />
+            </Layout>
+          }
+        >
+          <Route path="/" element={<CourseLandingPageJsx />} />
+          <Route path="/courses" element={<CoursesPage />} />
+          <Route path="/subject/:subjectId" element={<SubjectDetailsPage />} />
+        </Route>
       </Routes>
     </Router>
   );
