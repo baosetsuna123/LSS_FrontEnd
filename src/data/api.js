78679c715bf511c87f063001bf7515c1264938aa
ngo13082k2 <89001718+ngo13082k2@users.noreply.github.com>
2024-12-08 23:01:44 +0700

    Update api.js

--- src/data/api.js
+++ src/data/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "/api", // Use VITE_API_URL from environment variables or fall back to local proxy
-  withCredentials: true,
+  // withCredentials: true,
 });
 
 //Login api
